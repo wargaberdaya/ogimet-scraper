94ecf62f7df0b6efb990676b97d917dab68b8914
M. Zakiyuddin Munziri <zakiyuddinmunziri@gmail.com>
2024-11-13 20:05:06 +0800

    test: add more test

--- test/test_main.py
+++ test/test_main.py
@@ -1,13 +1,17 @@
 from typer.testing import CliRunner
 
-from main import app, fetch
+from main import app
 
-# runner = CliRunner()
+runner = CliRunner()
 
 
-def test_app():
-    # result = runner.invoke(app, ["fetch"])
+def test_summary_from():
+    result = runner.invoke(app, ["summary", "--from", "2024-11-13"])
+    assert result.exit_code == 0
 
-    # # assert result.exit_code == 0
 
-    fetch()
+def test_summary_from_to():
+    result = runner.invoke(
+        app, ["summary", "--from", "2024-11-13", "--to", "2024-11-15"]
+    )
+    assert result.exit_code == 0
